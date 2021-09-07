2f294056ab9ae2c45f42309fd1df5eae75c7ba64
SENURA PERERA <88277850+senura-47802@users.noreply.github.com>
2021-09-07 12:00:38 +0530

    Create circle.cpp

---
+++ circle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int r;
+ cout<<"What is the radius of circle:";
+ cin>>r;
+ cout<<"Circumstance of circle:"<<2*3.1428571429*r<<endl;
+
+    return 0;
+}
